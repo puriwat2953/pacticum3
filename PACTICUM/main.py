55f7ae585ccaa40d2b0eb687daf25bf36746e4c2
PURIWAT PAHUSUWANNO <puriwat.p@ku.th>
2025-12-26 15:40:50 +0700

    Add Flask news templates

---
+++ PACTICUM/main.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask import render_template, redirect, url_for
+from flask import request
+
+from datetime import datetime
+
+app = Flask(__name__)
+
+news_items = {
+    1: {'id': 1, 
+        'title': 'COVID-19 update', 
+        'body': 'COVID-19 is back'},
+    2: {'id': 2, 
+        'title': 'Facemasks found', 
+        'body': 'Recent news on facemask '},
+    3: {'id': 3,
+        'title':'Python 4', 
+        'body':'Python 4 will be out soon'},
+}
+
+@app.route('/')
+def index():
+    name = 'puriwat'
+    time = datetime.now()
+    return render_template('index.html', 
+                           name=name, 
+                           time=time,
+                           news_items=news_items.values())
+
+@app.route('/news/<id>/')
+def show_news_item(id):
+    news_item = news_items[int(id)]
+    return render_template('news_item.html',
+                           id=news_item['id'],
+                           title=news_item['title'],
+                           body=news_item['body'])
+
+def new_news_item(title, body):
+    new_id = max(news_items.keys()) + 1
+    return {
+        'id': new_id,
+        'title': title,
+        'body': body
+    }
+
+@app.route('/news/create/', methods=['POST'])
+def create_news_item():
+    item = new_news_item(request.form['title'],
+                         request.form['body'])
+    news_items[item['id']] = item
+    return redirect(url_for('index'))
